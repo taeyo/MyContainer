901744ce18b3378287fe37cf5b44486092c3a7c0
Taeyoung Kim <taeyokim@gmail.com>
2018-04-03 12:39:00 +0900

    Add container registry definition

--- main.tf
+++ main.tf
@@ -8,3 +8,18 @@ resource "azurerm_resource_group" "rg-taeyo-group" {
   name = "${var.arm-prefix}-group"
   location = "japaneast"
 }
+
+resource "azurerm_storage_account" "taeyostore" {
+  name = "taeyostore"
+  location = "japaneast"
+  resource_group_name = "${azurerm_resource_group.rke-group.name}"
+  account_replication_type = "LRS"
+  account_tier = "Standard"
+}
+
+resource "azurerm_container_registry" "taeyoegistry" {
+  name = "taeyoreg"
+  location = "japaneast"
+  resource_group_name = "${azurerm_resource_group.rke-group.name}"
+  storage_account_id = "${azurerm_storage_account.arkregstore.id}"
+}
